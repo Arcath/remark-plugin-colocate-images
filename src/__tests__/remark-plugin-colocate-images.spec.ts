df45a7b705af1f186455c8f3b6106467f3f6c91b
Adam Laycock <adam@arcath.net>
2021-04-22 12:46:29 +0100

    chore: tidy test code after jest messed it up

--- src/__tests__/remark-plugin-colocate-images.spec.ts
+++ src/__tests__/remark-plugin-colocate-images.spec.ts
@@ -1,81 +1,79 @@
-import fs from "fs";
-import path from "path";
-import remark from "remark";
-import html from "remark-html";
+import fs from 'fs'
+import path from 'path'
+import remark from 'remark'
+import html from 'remark-html'
 import rimraf from 'rimraf'
 import mkdirp from 'mkdirp'
 
-import { colocateImagesPlugin } from "../remark-plugin-colocate-images";
-
-const { readdir, unlink } = fs.promises;
+import {colocateImagesPlugin} from '../remark-plugin-colocate-images'
 
 describe("Co Locate Images", () => {
-  const BASE_DIR = path.join(process.cwd(), "src", "__tests__");
+  const BASE_DIR = path.join(process.cwd(), 'src', '__tests__')
 
   const clear = async () => {
-    if(fs.existsSync(path.join(BASE_DIR, "results"))){
+    if(fs.existsSync(path.join(BASE_DIR, 'results'))){
       await new Promise<void>((resolve) => {
-        rimraf(path.join(BASE_DIR, "results"), () => {
+        rimraf(path.join(BASE_DIR, 'results'), () => {
           resolve()
         })
       })
     }
 
-    await mkdirp(path.join(BASE_DIR, "results"))
-  };
+    await mkdirp(path.join(BASE_DIR, 'results'))
+  }
 
   beforeAll(async () => {
-    await clear();
-  });
+    await clear()
+  })
 
   afterEach(async () => {
-    await clear();
-  });
+    await clear()
+  })
 
-  it("should copy images to the correct path", async () => {
+  it('should copy images to the correct path', async () => {
     const markdown = `
 ![Image](./test.txt)
 
 ![External Image](http://example.com/image.jpg)
-    `.trim();
+    `.trim()
 
     const plugin = colocateImagesPlugin({
-      diskRoot: path.join(BASE_DIR, "samples"),
-      diskReplace: path.join(BASE_DIR, "results"),
-    });
+      diskRoot: path.join(BASE_DIR, 'samples'),
+      diskReplace: path.join(BASE_DIR, 'results'),
+    })
 
     const result = await remark()
       .use(html)
       .use(plugin as any)
-      .process(markdown);
+      .process(markdown)
 
     expect(result.toString()).toMatchInlineSnapshot(`
       "<p><img src=\\"/public/img/test.txt\\" alt=\\"Image\\"></p>
       <p><img src=\\"http://example.com/image.jpg\\" alt=\\"External Image\\"></p>
       "
-    `);
+    `)
 
-    const exists = fs.existsSync(path.join(BASE_DIR, "results", "test.txt"));
+    const exists = fs.existsSync(path.join(BASE_DIR, 'results', 'test.txt'))
 
-    expect(exists).toBeTruthy();
+    expect(exists).toBeTruthy()
   });
 
   it('should create directories as needed', async () => {
     const markdown = `
 ![Image](./test.txt)
-    `.trim();
+    `.trim()
 
     const plugin = colocateImagesPlugin({
-      diskRoot: path.join(BASE_DIR, "samples"),
-      diskReplace: path.join(BASE_DIR, "results", 'sample-post'),
-    });
+      diskRoot: path.join(BASE_DIR, 'samples'),
+      diskReplace: path.join(BASE_DIR, 'results', 'sample-post'),
+    })
 
     await remark()
       .use(plugin as any)
-      .process(markdown);
+      .process(markdown)
 
-    const exists = fs.existsSync(path.join(BASE_DIR, "results", "sample-post", "test.txt"));
+    const exists = fs.existsSync(path.join(BASE_DIR, 'results', 'sample-post', 'test.txt'))
 
-    expect(exists).toBeTruthy();
+    expect(exists).toBeTruthy()
   })
 });
